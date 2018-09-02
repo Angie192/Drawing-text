2e537f17629ab9068fd9404f8da06474d0262685
Angelina <Angie192>
2018-09-02 19:49:05 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Drawing-text
+# Drawing-text
---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.w3c.dom.*;
+import java.util.Random;
+
+
+
+public abstract class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Drawing Text");
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250, Color.WHITE);
+        Random rand = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 100; i++) {
+            int x = rand.nextInt((int) scene.getWidth());
+            int y = rand.nextInt((int) scene.getHeight());
+            int red = rand.nextInt(255);
+            int green = rand.nextInt(255);
+            int blue = rand.nextInt(255);
+
+            Text text = new Text(x, y, "hi");
+
+
+                int rot = rand.nextInt(360);
+                text.setFill(Color.rgb(red, green, blue));
+                text.setRotate(rot);
+                root.getChildren().add((javafx.scene.Node) text);
+            }
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+    }
