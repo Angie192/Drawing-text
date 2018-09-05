4be74b7e7cd4906f195666422ed8394ef20b25cc
Angie192 <angelina2esch@gmali.com>
2018-09-05 09:46:25 +0200

    Version 2.1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 
 
-public abstract class Main extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
